3bc7a9e3bf99ad96236e34d75c7931adf819e7b6
ImTotem <46699595+ImTotem@users.noreply.github.com>
2023-03-20 23:55:38 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 108 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.py
@@ -0,0 +1,9 @@
+import math
+from sys import stdin
+input = stdin.readline
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
