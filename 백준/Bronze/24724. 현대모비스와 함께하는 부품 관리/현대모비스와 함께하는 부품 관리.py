193d86ba6a173ac2b793ad08d2bd7454f01cc441
ImTotem <46699595+ImTotem@users.noreply.github.com>
2023-03-21 00:46:01 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 172 ms, Memory: 125416 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.py
@@ -0,0 +1,12 @@
+from sys import stdin
+input = stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    a, b = map(int, input().split())
+    for _ in range(n):
+        u, v = map(int, input().split())
+    print("Material Management", i+1)
+    print("Classification ---- End!")
