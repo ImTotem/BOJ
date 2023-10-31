fa4135f751873df7e3e6efb1fa11a5bce012ae00
Park Sungbin <46699595+ImTotem@users.noreply.github.com>
2023-11-01 01:26:58 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 111972 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,28 +1,16 @@
 import sys
 input = lambda:sys.stdin.readline().strip()
 
-class Stack:
-    def __init__(self): self.list = []
-        
-    def push(self, num): self.list.append(num)
+def main():
+    stack = []
+    for _ in range(int(input())):
+        cmd = input().split()
+        op = cmd[0]
     
-    def pop(self):
-        if not self.list: return -1
-        return self.list.pop()
-        
-    def size(self): return len(self.list)
-        
-    def empty(self): return int(not len(self.list))
-        
-    def top(self): return self.list[-1] if self.list else -1
+        if op == "push": stack.append(cmd[1])
+        elif op == "pop": print(stack.pop() if stack else -1)
+        elif op == "size": print(len(stack))
+        elif op == "empty": print(int(not stack))
+        else: print(stack[-1] if stack else -1)
 
-stack = Stack()
-for _ in range(int(input())):
-    cmd = input().split()
-    op = cmd[0]
- 
-    if op == "push": stack.push(cmd[1])
-    elif op == "pop": print(stack.pop())
-    elif op == "size": print(stack.size())
-    elif op == "empty": print(stack.empty())
-    else: print(stack.top())
+main()
