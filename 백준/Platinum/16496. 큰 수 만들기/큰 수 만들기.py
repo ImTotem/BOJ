5e0cd6139edfb6d519573b5f1ac36429b632401a
Park Sungbin <46699595+ImTotem@users.noreply.github.com>
2023-11-01 16:23:57 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,12 +1,2 @@
-import sys
-input = lambda:sys.stdin.readline().strip()
-from re import sub
-def main():
-    input()
-    a = input().split()
-    m = len(max(a, key=len))
-    a.sort(key=lambda x:x*m)
-
-    print(int(''.join(a[::-1])))
-
-main()
+input()
+print(int(''.join(sorted(input().split(),key=lambda x:x*9)[::-1])))
