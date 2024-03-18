02b0b388f2a435f07ae420ae8ccda3d9cd3dc7e2
Park Sungbin <46699595+ImTotem@users.noreply.github.com>
2024-03-18 09:02:28 +0900

    [Gold V] Title: :rightplant:, Time: 148 ms, Memory: 112160 KB -BaekjoonHub

--- 백준/Gold/31631. ：rightplant：/：rightplant：.py
+++ 백준/Gold/31631. ：rightplant：/：rightplant：.py
@@ -1,16 +1,14 @@
 import sys
 input = lambda:sys.stdin.readline().strip()
 
-from collections import deque
-
 def main():
     n = int(input())
 
-    dp = [[], [1]]
+    a, b = [[], [1]] 
 
     for i in range(2, n+1):
-        dp.append([i-1] + dp[i-2][::-1] + [i])
+        a, b = b, [i-1, *a[::-1], i]
     
-    return dp[n]
+    return b
     
 print(*main())
