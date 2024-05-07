fda1c9a41b8ec4b6b54fd725eadc40f7f8ad8e5f
박성빈 <46699595+ImTotem@users.noreply.github.com>
2024-05-07 16:56:13 +0900

    [Gold IV] Title: 스도쿠, Time: 1572 ms, Memory: 122388 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -2,23 +2,23 @@ import sys
 input = lambda:sys.stdin.readline().strip()
 
 board = [list(map(int, list(input()))) for _ in range(9)]
-zero = [(j, i) for i in range(9) for j in range(9) if board[i][j] == 0]
+zero = [(x, y) for y in range(9) for x in range(9) if board[y][x] == 0]
 
 def get(x, y):
-    ret = set(range(1, 10))
-
-    ret -= set(board[y])
-    ret -= set(board[y][x] for y in range(9))
+    bit = 0b1111111110
+    for i in board[y]:
+        bit &= ~(1<<i)
+    
+    for i in range(9):
+        bit &= ~(1<<board[i][x])
 
     seg_x, seg_y = 3*(x//3), 3*(y//3)
 
-    ret -= set(
-        board[i][j]
-        for i in range(seg_y, seg_y+3)
-        for j in range(seg_x, seg_x+3)
-    )
+    for i in range(seg_y, seg_y+3):
+        for j in range(seg_x, seg_x+3):
+            bit &= ~(1<<board[i][j])
 
-    return sorted(ret)
+    return bit
 
 def dfs(idx):
     if len(zero) == idx:
@@ -26,9 +26,11 @@ def dfs(idx):
         exit()
     
     x, y = zero[idx]
-    for i in get(x, y):
-        board[y][x] = i
-        dfs(idx+1)
-        board[y][x] = 0
+    bit = get(x, y)
+    for i in range(1, 10):
+        if bit>>i&1:
+            board[y][x] = i
+            dfs(idx+1)
+            board[y][x] = 0
 
 dfs(0)
