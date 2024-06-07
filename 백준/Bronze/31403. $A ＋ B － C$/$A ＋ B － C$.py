605d03a2a6bea2f3a182b7cc1876bfd7f7aea7d3
박성빈 <46699595+ImTotem@users.noreply.github.com>
2024-06-07 12:39:25 +0900

    [Bronze IV] Title: $A + B - C$, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,10 @@
+import sys
+input = lambda:sys.stdin.readline().strip()
+
+def main():
+    a, b, c = input(), input(), int(input())
+
+    print(int(a) + int(b) - c)
+    print(int(a+b) - c)
+
+main()
