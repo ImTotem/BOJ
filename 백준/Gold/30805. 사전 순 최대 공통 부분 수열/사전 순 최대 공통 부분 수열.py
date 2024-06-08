d1a9450ed93aa06ab241b088f814aabfecec8f01
박성빈 <46699595+ImTotem@users.noreply.github.com>
2024-06-09 00:27:17 +0900

    [Gold IV] Title: 사전 순 최대 공통 부분 수열, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
+++ 백준/Gold/30805. 사전 순 최대 공통 부분 수열/사전 순 최대 공통 부분 수열.py
@@ -1,25 +1,22 @@
 import sys
 input = lambda:sys.stdin.readline().strip()
 
-def main():
-    n = int(input())
-    a = list(map(int, input().split()))
-    m = int(input())
-    b = list(map(int, input().split()))
-    
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+a_set, b_set = set(a), set(b)
+intersec = a_set & b_set
+lcs = []
+while intersec:
+    num = max(intersec)
+    lcs.append(num)
+    ai, bi = a.index(num), b.index(num)
+    del a[:ai+1]
+    del b[:bi+1]
     a_set, b_set = set(a), set(b)
     intersec = a_set & b_set
-    lcs = []
-    while intersec:
-        num = max(intersec)
-        lcs.append(num)
-        ai, bi = a.index(num), b.index(num)
-        del a[:ai+1]
-        del b[:bi+1]
-        a_set, b_set = set(a), set(b)
-        intersec = a_set & b_set
 
-    print(len(lcs))
-    print(*lcs)
-
-main()
+print(len(lcs))
+print(*lcs)
