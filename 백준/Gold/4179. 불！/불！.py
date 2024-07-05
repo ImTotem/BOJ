09c21aea1e56d35c7856ec0f00c71c6142bb9538
박성빈 <46699595+ImTotem@users.noreply.github.com>
2024-07-05 13:38:24 +0900

    [Gold III] Title: 불!, Time: 536 ms, Memory: 172860 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -7,19 +7,21 @@ D = [0, 1, 0, -1, 0]
 
 def main():
     r, c = map(int, input().split())
-    maze = []
-    
+    maze = [list(input()) for _ in range(r)]
+
     q = deque()
     fire = deque()
+
+    visited = [[False] * c for _ in range(r)]
+
     for i in range(r):
-        maze.append(list(input()))
-        
         for j in range(c):
-            if maze[i][j] == 'J':
-                q.append((j, i, 0))
+            if maze[i][j] == 'F':
+                fire.append((j, i))
+            elif maze[i][j] == 'J':
                 maze[i][j] = '.'
-            if maze[i][j] == 'F': fire.append((j, i))
-    
+                q.append((j, i, 0))
+
     def spread():
         for _ in range(len(fire)):
             x, y = fire.popleft()
@@ -31,29 +33,26 @@ def main():
                     fire.append((nx, ny))
                     maze[ny][nx] = 'F'
 
-    visited = [[False] * c for _ in range(r)]
     visited[q[0][1]][q[0][0]] = True
 
-    pcnt = -1
+    prev = -1
     while q:
-        x, y, cnt = q.popleft()
+        x, y, sec = q.popleft()
 
-        if cnt != pcnt:
-            pcnt = cnt
+        if prev != sec:
+            prev = sec
             spread()
 
         if x in {0, c-1} or y in {0, r-1}:
-            return cnt + 1
+            return sec + 1
 
         for i in range(4):
-            nx, ny, ncnt = x + D[i], y + D[i+1], cnt + 1
+            nx, ny = x + D[i], y + D[i+1]
 
             if 0 <= nx < c and 0 <= ny < r and maze[ny][nx] == '.' and not visited[ny][nx]:
                 visited[ny][nx] = True
-                q.append((nx, ny, ncnt))
-
-        
-    return "IMPOSSIBLE"
+                q.append((nx, ny, sec+1))
     
+    return "IMPOSSIBLE"
 
 print(main())
