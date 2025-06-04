5587dfcb3d55e8d43ded1dc9066c22abc8b683b7
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-06-04 20:17:32 +0900

    [Silver I] Title: 지름길, Time: 112 ms, Memory: 110068 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,7 +1,6 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
 from collections import defaultdict
-from heapq import heappop, heappush
 
 INF = float('inf')
 
@@ -10,29 +9,15 @@ def main():
     graph = defaultdict(list)
     for _ in range(n):
         start, end, cost = map(int, input().split())
-        if end <= d:
-            graph[start].append((cost, end))
+        graph[end].append((start, cost))
     
-    for i in range(d):
-        graph[i].append((1, i + 1))
-        
-    dist = [INF] * (d + 1)
-    dist[0] = 0
+    dp = [0] * (d + 1)
+    for i in range(1, d+1):
+        dp[i] = dp[i-1] + 1
+        for start, cost in graph[i]:
+            dp[i] = min(dp[i], dp[start] + cost)
 
-    pq = [(0, 0)]
-    while pq:
-        ucost, u = heappop(pq)
-        
-        if ucost > dist[u]:
-            continue
-
-        for vcost, v in graph[u]:
-            ncost = ucost + vcost
-            if ncost < dist[v]:
-                dist[v] = ncost
-                heappush(pq, (ncost, v))
-
-    return dist[d]
+    return dp[d]
 
 if __name__ == "__main__":
     print(main())
