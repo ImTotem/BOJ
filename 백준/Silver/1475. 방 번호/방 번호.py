d627fdf83cbac5263a1e928bdb149b27e35b41cd
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-03 17:02:37 +0900

    [Silver V] Title: 방 번호, Time: 108 ms, Memory: 110064 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -6,12 +6,11 @@ from math import ceil
 INF = float('inf')
 
 def main():
-    n = input()
+    n = input().replace('6', '9')
     cnt = Counter(n)
-    ans = max(cnt[str(i)] for i in range(10) if i not in {6, 9})
-    ans = max(ans, ceil((cnt['6'] + cnt['9']) / 2))
+    ans = max(cnt[str(i)] for i in set(range(10))-{9})
 
-    return ans
+    return max(ans, ceil(cnt['9'] / 2))
 
 
 if __name__ == "__main__":
