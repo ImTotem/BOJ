7a825ba920452c59858711b5dd4ee87c022c0850
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-04 11:07:20 +0900

    [Silver II] Title: 예산, Time: 100 ms, Memory: 110788 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,17 +1,23 @@
 import sys
-
 input = lambda: sys.stdin.readline().strip()
 
-n = int(input())
-budget = list(map(int, input().split()))
-budget.sort()
-m = int(input())
+def main():
+    n = int(input())
+    budgets = list(map(int, input().split()))
+    m = int(input())
 
-answer = float('-inf')
+    lo, hi = 1, max(budgets)
+    while lo <= hi:
+        mid = (lo + hi) // 2
 
-sum_budget = 0
-for i in range(n):
-    answer = max(answer, (m-sum_budget) // (n-i) )
-    sum_budget += budget[i]
+        budget = sum(min(i, mid) for i in budgets)
 
-print(min(answer, budget[-1]))
+        if budget > m:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    
+    return hi
+    
+if __name__ == "__main__":
+    print(main())
