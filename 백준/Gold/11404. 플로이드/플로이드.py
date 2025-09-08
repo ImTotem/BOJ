e370f4613ade8bbb32808ba6c954358461f3f07e
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-08 23:15:01 +0900

    [Gold IV] Title: 플로이드, Time: 156 ms, Memory: 111592 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,23 +1,27 @@
-INF = float("inf")
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().strip()
+from heapq import heappop, heappush
 
-n = int(input())
-m = int(input())
+INF = float('inf')
 
-graph = [[[0, INF][i!=j] for j in range(n)] for i in range(n)]
+def main():
+    n = int(input())
+    m = int(input())
 
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a-1][b-1] = min(graph[a-1][b-1], c)
-
-for k in range(n):
+    graph = [[INF] * n for _ in range(n)]
     for i in range(n):
-        for j in range(n):
-            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+        graph[i][i] = 0
 
-for i in range(n):
-    for j in range(n):
-        graph[i][j] = graph[i][j] if graph[i][j] != INF else 0
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a-1][b-1] = min(graph[a-1][b-1], c)
     
-    print(*graph[i])
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    
+    return '\n'.join(' '.join(map(lambda x:str([x, 0][x==INF]), i)) for i in graph)
+    
+if __name__ == "__main__":
+    print(main())
