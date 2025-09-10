9f33ebea3ccfa3397e5805182a72975336e140b7
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-11 00:03:11 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 112008 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,29 +1,27 @@
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().strip()
+from collections import Counter
 
-n, m = map(int, input().split())
+INF = float('inf')
 
-board = [input() for _ in range(n)]
+def main():
+    n, m = map(int, input().split())
+    board = [input() for _ in range(n)]
 
-ans = []
+    ans = INF
+    
+    for i in range(n - 7):
+        for j in range(m - 7):
+            a, b = 0, 0
+            for k in range(i, i + 8):
+                for l in range(j, j + 8):
+                    a += board[k][l] != "WB"[(k+l)%2]
+                    b += board[k][l] != "BW"[(k+l)%2]
+            
+            ans = min(ans, a, b)
+    
+    return ans
 
-for i in range(n-7):
-    for j in range(m-7):
-        idx1 = 0
-        idx2 = 0
-        
-        for k in range(i, i+8):
-            for l in range(j, j+8):
-                if ( k+l )%2 == 0:
-                    if board[k][l] != "W":
-                        idx1 += 1
-                    if board[k][l] != "B":
-                        idx2 += 1
-                else:
-                    if board[k][l] != "B":
-                        idx1 += 1
-                    if board[k][l] != "W":
-                        idx2 += 1
-        ans.append(min(idx1, idx2))
-
-print(min(ans))
+    
+if __name__ == "__main__":
+    print(main())
