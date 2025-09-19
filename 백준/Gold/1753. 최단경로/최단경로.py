abf1b5787c6238addcd3f669a00ce65ad4318b56
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-19 18:58:53 +0900

    [Gold IV] Title: 최단경로, Time: 412 ms, Memory: 136304 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,40 +1,35 @@
-from heapq import heappop as pop, heappush as push
-INF = float("inf")
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().strip()
+from heapq import heappop, heappush
 
-V, E = map(int, input().split())
+INF = float('inf')
 
-K = int(input())
+def main():
+    v, e = map(int, input().split())
+    k = int(input())
+    graph = [[] for _ in range(v + 1)]
+    
+    ans = [INF] * (v + 1)
+    ans[k] = 0
 
-graph = [[] for _ in range(V+1)]
-
-for i in range(E):
-    u, v, w = map(int, input().split())
-    graph[u].append([w, v])
-
-def dijkstra(start):
-    pq = []
-    visited = [INF] * (V+1)
-    push(pq, (0, start))
-    visited[start] = 0
-
+    for _ in range(e):
+        a, b, c = map(int, input().split())
+        graph[a].append((c, b))
+    
+    pq = [(0, k)]
     while pq:
-        wu, u = pop(pq)
-
-        if visited[u] < wu:
+        c, a = heappop(pq)
+        
+        if ans[a] < c:
             continue
 
-        for wv, v in graph[u]:
-            wuv = wu + wv
-
-            if visited[v] > wuv:
-                push(pq, [wuv, v])
-                visited[v] = wuv
-        
-    return visited
-
-visited = dijkstra(K)
-
-for i in range(1, V+1):
-    print(["INF", visited[i]][visited[i] != INF])
+        for bc, b in graph[a]:
+            cc = bc + c
+            if ans[b] > cc:
+                heappush(pq, (cc, b))
+                ans[b] = cc
+    
+    return '\n'.join(str(ans[i]).upper() for i in range(1, v+1))
+    
+if __name__ == "__main__":
+    print(main())
