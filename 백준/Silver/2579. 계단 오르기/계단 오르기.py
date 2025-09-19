f2dfe971eadbe3fb1a63d681f52ef76554e98bbd
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-19 19:05:52 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,19 +1,19 @@
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().strip()
 
-n = int(input())
-s = [int(input()) for _ in range(n)]
+INF = float('inf')
 
-if n < 3:
-    print(sum(s))
-else:
-    dp = []
+def main():
+    n = int(input())
+    s = [int(input()) for _ in range(n)]
+    if n == 1: return s[0]
 
-    dp.append(s[0])
-    dp.append(s[0]+s[1])
-    dp.append(max(s[0] + s[2], s[1]+s[2]))
+    dp = [s[0], s[0] + s[1]] + [0] * (n - 2)
 
-    for i in range(3, n):
-        dp.append(max( s[i] + dp[i-2], dp[i-3] + s[i-1] + s[i] ))
+    for i in range(2, n):
+        dp[i] = s[i] + max(dp[i-2], s[i-1] + dp[i-3])
 
-    print(dp[-1])
+    return dp[-1]
+    
+if __name__ == "__main__":
+    print(main())
