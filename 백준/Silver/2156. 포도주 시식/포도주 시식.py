4eb85b4ce6dc0101b6a2e3e210db322795a0f479
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-19 19:05:45 +0900

    [Silver I] Title: 포도주 시식, Time: 112 ms, Memory: 110988 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,19 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+INF = float('inf')
+
+def main():
+    n = int(input())
+    s = [int(input()) for _ in range(n)]
+    if n == 1: return s[0]
+
+    dp = [s[0], s[0] + s[1]] + [0] * (n - 2)
+
+    for i in range(2, n):
+        dp[i] = max(dp[i-1], s[i] + max(dp[i-2], s[i-1] + dp[i-3]))
+
+    return dp[-1]
+    
+if __name__ == "__main__":
+    print(main())
