a593c083e40fb3ab7fbd3a3375d3abcffaec369b
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-19 19:06:00 +0900

    [Silver I] Title: RGB거리, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,15 +1,18 @@
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().strip()
 
-N = int(input())
-rgb = [list(map(int, input().split())) for _ in range(N)]
+INF = float('inf')
 
-dp = [[0,0,0] for _ in range(N)]
-dp[0] = rgb[0]
-
-for i in range(1, N):
-    dp[i][0] = rgb[i][0] + min(dp[i-1][1], dp[i-1][2])
-    dp[i][1] = rgb[i][1] + min(dp[i-1][0], dp[i-1][2])
-    dp[i][2] = rgb[i][2] + min(dp[i-1][0], dp[i-1][1])
-
-print(min(dp[N-1]))
+def main():
+    n = int(input())
+    dp = [list(map(int, input().split())) for _ in range(n)]
+    
+    for i in range(1, n):
+        dp[i][0] += min(dp[i-1][1], dp[i-1][2])
+        dp[i][1] += min(dp[i-1][0], dp[i-1][2])
+        dp[i][2] += min(dp[i-1][0], dp[i-1][1])
+    
+    return min(dp[-1])
+    
+if __name__ == "__main__":
+    print(main())
