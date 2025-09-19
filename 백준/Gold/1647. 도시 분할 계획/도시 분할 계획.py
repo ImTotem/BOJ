b65ac950b2b82c44588b1a51fc5521da5d581506
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-19 19:03:53 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2020 ms, Memory: 264040 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -4,37 +4,36 @@ input = lambda: sys.stdin.readline().strip()
 INF = float('inf')
 
 def main():
-    def find(v):
-        if parent[v] == v: return v
-        parent[v] = find(parent[v])
-        return parent[v]
-
-    def union(u, v):
-        root_u = find(u)
-        root_v = find(v)
-
-        if rank[root_u] > rank[root_v]:
-            parent[root_v] = root_u
-            rank[root_u] += 1
-        else:
-            parent[root_u] = root_v
-            rank[root_v] += 1
-
     n, m = map(int, input().split())
-    graph = [tuple(map(int, input().split())) for _ in range(m)]
-    graph.sort(key=lambda x:x[2])
+    edges = [tuple(map(int, input().split())) for _ in range(m)]
+    edges.sort(key=lambda x:x[2])
 
     parent = list(range(n + 1))
     rank = [0] * (n + 1)
-    
-    ans = []
 
-    for a, b, c in graph:
-        if find(a) != find(b):
-            ans.append(c)
-            union(a, b)
+    def union(a, b):
+        a, b = find(a), find(b)
+        a, b = (b, a) if rank[a] > rank[b] else (a, b)
+        
+        parent[a] = b
+        rank[b] += 1
     
-    return sum(ans) - ans[-1]
+    def find(x):
+        if x == parent[x]: return x
+        parent[x] = find(parent[x])
+        return parent[x]
+
+    ans = 0
+    maxi = 0
+    for a, b, c in edges:
+        if find(a) == find(b): continue
+
+        union(a, b)
+
+        ans += c
+        maxi = max(maxi, c)
+        
+    return ans - maxi
 
 if __name__ == "__main__":
     print(main())
