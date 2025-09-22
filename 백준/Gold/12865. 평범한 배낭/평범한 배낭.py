ad9011753800c7d5364f3881e04c50889f82f9cb
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-22 11:42:19 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 110328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,22 @@
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().strip()
 
-N, K = map(int, input().split()) # N: 물품의 수, K: 무게
-obj = [[0, 0]]+[list(map(int, input().split())) for _ in range(N)]
-# obj = [ [무게, 가치], ... ]
+INF = float('inf')
 
-dp = [0] * (K+1)
+def main():
+    n, k = map(int, input().split())
+    items = [tuple(map(int, input().split())) for _ in range(n)]
+    items.sort()
 
-for i in range(1, N+1):
-    for j in range(K, 0, -1):
-        if obj[i][0] <= j:
-            dp[j] = max(dp[j], dp[j - obj[i][0]] + obj[i][1])
+    dp = [0] * (k + 1)
+    
+    for w, v in items:
+        for i in range(k, 0, -1):
+            if w > i: continue
+            dp[i] = max(dp[i], dp[i - w] + v)
+    
+    return dp[k]
 
-print(dp[K])
+
+if __name__ == "__main__":
+    print(main())
