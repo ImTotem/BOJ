dcda25d3cae315714829ec71b3a69a22ce7a655f
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-09-25 10:28:34 +0900

    [Gold III] Title: 소수의 연속합, Time: 644 ms, Memory: 268212 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,33 +1,40 @@
 import sys
-input = lambda:sys.stdin.readline().strip()
+input = lambda: sys.stdin.readline().strip()
+from itertools import accumulate
 
-def get_primes(n):
-    is_prime = [False, False] + [True] * (n-1)
-    ret = [0]
-    
-    for i in range(2, n+1):
-        if is_prime[i]:
-            ret.append(ret[-1] + i)
-            for j in range(2*i, n+1, i): is_prime[j] = False
-
-    return ret
+INF = float('inf')
 
 def main():
-    cnt = 0
-
     n = int(input())
-    acc = get_primes(n)
-    N = len(acc)
-    
-    l, r = 0, 1
-    while r < N:
-        cur = acc[r] - acc[l]
-        if cur < n: r += 1
-        elif n < cur: l += 1
-        else:
-            cnt += 1
-            l += 1
-            r += 1
 
-    print(cnt)
-main()
+    MAX = 4_000_000 + 1
+    rset = lambda start, end, gap: set(range(start, end, gap))
+
+    prime = rset(5, MAX, 6) | rset(7, MAX, 6)
+    prime.add(2)
+    prime.add(3)
+    
+    for i in range(5, int(MAX ** .5) + 1, 6):
+        if i in prime:
+            prime -= rset(i * i, MAX, i * 6) | rset(i * (i + 2), MAX, i * 6)
+        j = i + 2
+        if j in prime:
+            prime -= rset(j * j, MAX, j * 6) | rset(j * (j + 4), MAX, j * 6)
+    
+    prime = list(prime)
+    prime.sort()
+    acc = list(accumulate(prime)) + [0]
+    m = len(prime)
+    
+    ans = 0
+    l = -1
+    for r in range(m):
+        if prime[r] > n: break
+        while acc[r] - acc[l] >= n:
+            ans += (acc[r] - acc[l] == n)
+            l += 1
+    
+    return ans
+
+if __name__ == "__main__":
+    print(main())
