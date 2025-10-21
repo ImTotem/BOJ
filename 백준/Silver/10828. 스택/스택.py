3a29e1edaf7220b3b2e7daf51e659650dd167a23
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-10-21 19:15:58 +0900

    [Silver IV] Title: 스택, Time: 136 ms, Memory: 111664 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,16 +1,24 @@
 import sys
 input = lambda:sys.stdin.readline().strip()
 
+class Stack():
+    def __init__(self): self.stack = []
+    def push(self, n): self.stack.append(n)
+    def pop(self): return self.stack.pop() if self.stack else -1
+    def size(self): return len(self.stack)
+    def empty(self): return int(not self.stack)
+    def top(self): return self.stack[-1] if self.stack else -1
+
 def main():
-    stack = []
+    stack = Stack()
     for _ in range(int(input())):
         cmd = input().split()
         op = cmd[0]
     
-        if op == "push": stack.append(cmd[1])
-        elif op == "pop": print(stack.pop() if stack else -1)
-        elif op == "size": print(len(stack))
-        elif op == "empty": print(int(not stack))
-        else: print(stack[-1] if stack else -1)
+        if op == "push": stack.push(cmd[1])
+        elif op == "pop": print(stack.pop())
+        elif op == "size": print(stack.size())
+        elif op == "empty": print(stack.empty())
+        else: print(stack.top())
 
 main()
