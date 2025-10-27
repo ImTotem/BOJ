9a3fa2bcdae09ddc9d9c277ea2c647757ad8cac0
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-10-27 11:50:35 +0900

    [Silver IV] Title: 평행사변형, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1064. 평행사변형/평행사변형.py
@@ -0,0 +1,24 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+INF = float('inf')
+
+def main():
+    ax, ay, bx, by, cx, cy = map(int, input().split())
+    a, b, c = (ax, ay), (bx, by), (cx, cy)
+
+    if ax == bx == cx: return -1
+    
+    if (by - ay) * (cx - bx) == (cy - by) * (bx - ax) : return -1
+
+    ss = lambda p, q: (p - q) ** 2
+    dist = lambda p, q: (ss(p[0], q[0]) + ss(p[1], q[1])) ** .5
+    f = lambda p, q, r: 2 * (dist(p, q) + dist(q, r))
+    
+    li = [f(a, b, c), f(b, c, a), f(c, a, b)]
+
+    return max(li) - min(li)
+
+
+if __name__ == "__main__":
+    print(main())
