c53ed0bc352c0356ad1a6e5f3da71b3857de819c
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-11-05 17:25:57 +0900

    [Platinum III] Title: 삶의 질, Time: 6796 ms, Memory: 208600 KB, Score: 60 point -BaekjoonHub

--- 백준/Platinum/10227. 삶의 질/삶의 질.py
+++ 백준/Platinum/10227. 삶의 질/삶의 질.py
@@ -1,6 +1,5 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
-from collections import Counter
 
 INF = float('inf')
 
@@ -8,12 +7,21 @@ def main():
     r, c, h, w = map(int, input().split())
     city = [list(map(int, input().split())) for _ in range(r)]
 
-    mid = h * w // 2
-    
+    maxi = r * c
+    mid = (h * w) // 2
+
     def ps(x, y):
-        seg = [city[y + dy][x + dx] for dy in range(h) for dx in range(w)]
-        seg.sort()
-        return seg[mid]
+        cs = [0] * (maxi + 1)
+        for dy in range(h):
+            for dx in range(w):
+                cs[city[y + dy][x + dx]] += 1
+
+        cnt = 0
+        for i in range(1, maxi + 1):
+            cnt += cs[i]
+            if cnt > mid:
+                return i
+
     
     return min(ps(x, y) for y in range(r - h + 1) for x in range(c - w + 1))
 
