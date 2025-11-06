88b133f7dea98226a0cf9a773ba7d3a6d1bf4b06
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-11-06 16:09:18 +0900

    [Platinum III] Title: 삶의 질, Time: 8840 ms, Memory: 343272 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/10227. 삶의 질/삶의 질.py
+++ 백준/Platinum/10227. 삶의 질/삶의 질.py
@@ -6,24 +6,35 @@ INF = float('inf')
 def main():
     r, c, h, w = map(int, input().split())
     city = [list(map(int, input().split())) for _ in range(r)]
+    mask = [[0 for _ in range(c + 1)] for _ in range(r + 1)]
+    acc = [[0 for _ in range(c + 1)] for _ in range(r + 1)]
+    
+    def ps(m):
+        for y in range(r):
+            for x in range(c):
+                mask[y][x] = (-1, 1)[city[y][x] > m]
+                
+        for y in range(r):
+            for x in range(c):
+                acc[y][x] = acc[y-1][x] + acc[y][x-1] - acc[y-1][x-1] + mask[y][x]
 
-    maxi = r * c
-    mid = (h * w) // 2
-
-    def ps(x, y):
-        cs = [0] * (maxi + 1)
-        for dy in range(h):
-            for dx in range(w):
-                cs[city[y + dy][x + dx]] += 1
-
-        cnt = 0
-        for i in range(1, maxi + 1):
-            cnt += cs[i]
-            if cnt > mid:
-                return i
+        for y in range(h - 1, r):
+            for x in range(w - 1, c):
+                if acc[y][x] - acc[y-h][x] - acc[y][x-w] + acc[y-h][x-w] <= 0:
+                    return True
+        return False
 
     
-    return min(ps(x, y) for y in range(r - h + 1) for x in range(c - w + 1))
+    lo, hi = 1, r * c
+    while lo <= hi:
+        mid = (lo + hi) // 2
+
+        if ps(mid):
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    
+    return lo
 
 
 if __name__ == "__main__":
