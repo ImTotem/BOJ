876528bdd81c7e3efd0f1261dfa4f786db587670
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-11-07 22:05:24 +0900

    [Silver I] Title: 스타트링크, Time: 192 ms, Memory: 125780 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,5 +1,6 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
+
 from collections import deque
 
 INF = float('inf')
@@ -8,22 +9,23 @@ def main():
     f, s, g, u, d = map(int, input().split())
     d = -d
 
-    visited = set()
+    visited = [0] * (f + 1)
+    visited[s] = 1
     q = deque([(s, 0)])
+
     while q:
-        s, cnt = q.popleft()
+        x, cnt = q.popleft()
 
-        if s == g: return cnt
+        if x == g: return cnt
 
-        for i in u, d:
-            ns = s + i
+        for m in u, d:
+            nx = x + m
+            if not (0 < nx <= f): continue
+            if visited[nx]: continue
+            q.append((nx, cnt + 1))
+            visited[nx] = 1
 
-            if 1 <= ns <= f and ns not in visited:
-                visited.add(ns)
-                q.append((ns, cnt + 1))
-
-    return "use the stairs"
-    
+    return 'use the stairs'
 
 if __name__ == "__main__":
     print(main())
