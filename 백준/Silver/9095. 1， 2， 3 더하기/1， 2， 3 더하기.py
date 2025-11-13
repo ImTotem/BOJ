b83e4a394ae61393297ca1d7d041d7ac34e709a6
박성빈 <46699595+ImTotem@users.noreply.github.com>
2025-11-13 16:09:27 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,5 +1,17 @@
-from sys import stdin
-input = stdin.readline
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-for _ in range(int(input())):
-    print([1, 2, 4, 7, 13, 24, 44, 81, 149, 274][int(input())-1])
+INF = float('inf')
+
+def main():
+    n = int(input())
+    dp = [0, 1, 2, 4]
+    
+    for _ in range(4, n + 1):
+        dp.append(dp[-1] + dp[-2] + dp[-3])
+    
+    return dp[n]
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        print(main())
