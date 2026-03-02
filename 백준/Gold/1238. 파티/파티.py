1f6cb7797cdd8b1ef133d3eadafc0c7f9b7c8281
박성빈 <46699595+ImTotem@users.noreply.github.com>
2026-03-02 22:54:37 +0900

    [Gold III] Title: 파티, Time: 1068 ms, Memory: 118648 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,37 +1,38 @@
-from heapq import heappop, heappush
-
 import sys
-input = lambda:sys.stdin.readline().strip()
+input = lambda: sys.stdin.readline().strip()
+from heapq import heappop, heappush
 
 INF = float('inf')
 
-n, m, x = map(int, input().split())
-
-graph = [[] for _ in range(n+1)]
-
-for _ in range(m):
-    u, v, t = map(int, input().split())
-    graph[u].append((t, v))
-
-def dijkstra(start, end):
-    pq = []
-    visited = [INF] * (n+1)
-    heappush(pq, (0, start))
-    visited[start] = 0
-
-    while pq:
-        cur_cost, cur_node = heappop(pq)
-
-        if visited[cur_node] < cur_cost:
-            continue
-
-        for nxt_cost, nxt_node in graph[cur_node]:
-            sum_cost = cur_cost + nxt_cost
-
-            if visited[nxt_node] > sum_cost:
-                heappush(pq, (sum_cost, nxt_node))
-                visited[nxt_node] = sum_cost
+def main():
+    n, m, x = map(int, input().split())
+    graph = [[] for _ in range(n)]
     
-    return visited[end]
+    for _ in range(m):
+        a, b, t = map(int, input().split())
+        graph[a-1].append((t, b-1))
+    
+    def dijkstra(x, y):
+        pq = [(0, x)]
+        visited = [INF] * (n + 1)
+        visited[x] = 0
+        while pq:
+            t, a = heappop(pq)
 
-print(max(dijkstra(i+1, x) + dijkstra(x, i+1) for i in range(n)))
+            if visited[a] < t:
+                continue
+
+            for bt, b in graph[a]:
+                nt = t + bt
+
+                if visited[b] > nt:
+                    heappush(pq, (nt, b))
+                    visited[b] = nt
+        
+        return visited[y]
+
+    return max(dijkstra(x-1, y) + dijkstra(y, x-1) for y in range(n))
+
+    
+if __name__ == "__main__":
+    print(main())
