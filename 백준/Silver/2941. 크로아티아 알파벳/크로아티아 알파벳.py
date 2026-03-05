613f5199934e308abd297fc6122454eb3ce4d722
박성빈 <46699595+ImTotem@users.noreply.github.com>
2026-03-06 00:30:20 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 112 ms, Memory: 110064 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,1 +1,18 @@
-print(len(input().replace("c=","0").replace("c-","0").replace("dz=","0").replace("d-","0").replace("lj","0").replace("nj","0").replace("s=","0").replace("z=","0")))
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+from collections import defaultdict, deque
+
+INF = float('inf')
+D = [0, 1, 0, -1, 0]
+
+def main():
+    a = input()
+
+    rep = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+    for r in rep:
+        a = a.replace(r, '0')
+
+    return len(a)
+
+if __name__ == "__main__":
+    print(main())
