b6455a3f3c7e3cc724c1150161f96b451925337b
박성빈 <46699595+ImTotem@users.noreply.github.com>
2026-03-06 10:59:50 +0900

    [Gold III] Title: 개미굴, Time: 104 ms, Memory: 111276 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.py
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -1,34 +1,31 @@
 import sys
-input = lambda: sys.stdin.readline().strip()
-from itertools import accumulate
-from collections import Counter, deque, defaultdict
-from heapq import heapify, heappop, heappush
-from bisect import bisect_left
+input = lambda: sys.stdin.readline().rstrip()
 
 INF = float('inf')
+D = [0, 1, 0, -1, 0]
 
 def main():
-    ans = []
+    n = int(input())
 
     trie = dict()
 
-    n = int(input())
     for _ in range(n):
-        _, *a = input().split()
+        _, *t = input().split()
         
         nxt = trie
-        for w in a:
+        for w in t:
             if w not in nxt:
                 nxt[w] = dict()
             nxt = nxt[w]
-
+    
+    ans = []
     def dfs(trie, depth):
         if not trie: return
 
         for w in sorted(trie):
             ans.append('--' * depth + w)
             dfs(trie[w], depth + 1)
-
+    
     dfs(trie, 0)
 
     return '\n'.join(ans)
